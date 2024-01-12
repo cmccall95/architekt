a437e5aaceccf5582a8877eb655d186b2edfd573
Denilson Amaral <denilson19282@gmail.com>
2024-01-12 10:59:04 -0300

    fix/fix aspect ratio

--- lib/modules/presentation/pages/blueprint/widgets/blueprint_pdf.dart
+++ lib/modules/presentation/pages/blueprint/widgets/blueprint_pdf.dart
@@ -1,3 +1,4 @@
+import 'package:arkitekt/core/config/logger_custom.dart';
 import 'package:arkitekt/modules/presentation/pages/blueprint/widgets/blueprint_columns.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -41,6 +42,19 @@ class BlueprintPdf extends StatelessWidget {
 class _Blueprint extends StatelessWidget {
   const _Blueprint({super.key});
 
+  Future<(double, double)> _getPdfSize() async {
+    final controller = Get.find<BlueprintPdfController>();
+    final pdfController = controller.pdfController.value;
+    if (pdfController == null) {
+      return (0.0, 0.0);
+    }
+
+    final doc = await pdfController.document;
+    final page = await doc.getPage(1);
+
+    return (page.width, page.height);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Obx(() {
@@ -50,32 +64,39 @@ class _Blueprint extends StatelessWidget {
         return const SizedBox.shrink();
       }
 
-      return AspectRatio(
-        aspectRatio: 1224.0 / 792.0,
-        child: Container(
-          color: Colors.green.withOpacity(0.2),
-          child: LayoutBuilder(builder: (context, constraints) {
-            final canvasWidth = constraints.maxWidth;
-            final canvasHeight = constraints.maxHeight;
+      return FutureBuilder(
+          future: _getPdfSize(),
+          builder: (context, snapshot) {
+            return AspectRatio(
+              aspectRatio: snapshot.hasData
+                  ? snapshot.data!.$1 / snapshot.data!.$2
+                  : 1.0,
+              child: Container(
+                color: Colors.green.withOpacity(0.2),
+                child: LayoutBuilder(builder: (context, constraints) {
+                  final canvasWidth = constraints.maxWidth;
+                  final canvasHeight = constraints.maxHeight;
 
-            return Stack(
-              children: [
-                InteractiveViewer(
-                  child: PdfView(
-                    controller: pdfController,
-                    physics: const NeverScrollableScrollPhysics(),
-                    onPageChanged: (newPage) => controller.page.value = newPage,
-                  ),
-                ),
-                BlueprintCanvas(
-                  width: canvasWidth,
-                  height: canvasHeight,
-                ),
-              ],
+                  return Stack(
+                    children: [
+                      InteractiveViewer(
+                        child: PdfView(
+                          controller: pdfController,
+                          physics: const NeverScrollableScrollPhysics(),
+                          onPageChanged: (newPage) =>
+                              controller.page.value = newPage,
+                        ),
+                      ),
+                      BlueprintCanvas(
+                        width: canvasWidth,
+                        height: canvasHeight,
+                      ),
+                    ],
+                  );
+                }),
+              ),
             );
-          }),
-        ),
-      );
+          });
     });
   }
 }
